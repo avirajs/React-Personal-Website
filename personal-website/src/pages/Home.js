3ffdc699a9c2e51aecb420ec21113126064ff9f9
Aviraj Sinha <avirajs@Avirajs-MacBook-Pro.local>
2022-11-25 09:59:25 -0600

    New app with all home, skills pages

---
+++ personal-website/src/pages/Home.js
@@ -0,0 +1,50 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Avi Sinha</h2>
+        <div className="prompt">
+          <p>A computer scientist with interest with machine learning and quantum information.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Research</h2>
+            <span>
+              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
+              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Data Science</h2>
+            <span>
+              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
+              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+            </span>
+          </li>
+          <li className="item">
+            <h2>Software Development</h2>
+            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+          </li>
+          <li className="item">
+            <h2>Other Platforms and Technologies</h2>
+            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ personal-website/src/pages/Projects.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import { ProjectList } from "../helpers/ProjectList";
+
+import "../styles/Projects.css";
+
+function Projects() {
+  return (
+    <div>
+    <div className="projects">
+      <h1> Work Projects </h1>
+      <div className="projectList">
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
+        })}
+      </div>
+    </div>
+
+    <div className="projects">
+      <h1> Publications </h1>
+      <div className="projectList">
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
+        })}
+      </div>
+    </div>
+
+    <div className="projects">
+      <h1> Personal Projects </h1>
+      <div className="projectList">
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
+        })}
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Projects;
